d16912e8adea71ef5cfd7b096509183dca538b49
Safak <safak.kocaoglu@bil.omu.edu.tr>
2014-11-03 00:38:49 +0200

    Revert "Revert "regular""
    This reverts commit dcb3a4baf26c2e28fca23cbd8c5a5b84bdd08cd4.

--- ReplaceUnderscoreWithSpace.sh
+++ ReplaceUnderscoreWithSpace.sh
@@ -3,4 +3,4 @@
  
 string=$1
 [[ $string =~ ^(_*)(.*[^_])(_*)$ ]]
-echo "${BASH_REMATCH[1]}${BASH_REMATCH[2]//_/ }${BASH_REMATCH[3]}
+echo "${BASH_REMATCH[1]}${BASH_REMATCH[2]//_/ }${BASH_REMATCH[3]}"
