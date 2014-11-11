b2e0ed0968120f0bafa038e458374ff5a13c317c
Safak <safak.kocaoglu@bil.omu.edu.tr>
2014-11-11 15:30:34 +0200

    boşluk düzelt

--- ReplaceUnderscoreWithSpace2.sh
+++ ReplaceUnderscoreWithSpace2.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
+#döngü ile hesap
 function ReplaceUnderscoreWithSpace()
 {
 
-len=${#string}
+len= ${#string}
 i=0
 
 while [ $i -lt $len ]; do
- dizi[$i]=${string:$i:1}
- i=$((i+1))
+ dizi[$i] = ${string:$i:1}
+ i= $((i+1))
 done
 
 for ((a=0; a<$len; a++)); do
@@ -18,20 +19,20 @@ done
 
 for (( b=$((len-1)); b>0; b-- )); do
  if [[ "${dizi[$b]}" != "_" ]]; then
-  b=${b}
+  b= ${b}
   break
  fi
 done
 
 for (( k=$((a+1)); k<$b; k++ )); do	
  if [[ "${dizi[$k]}" = "_" ]]; then
-  dizi[$k]=" "
+  dizi[$k]= " "
  else
   :
  fi
 done
 printf "%s" "${dizi[@]}"
 }
-string=$1
+string= $1
 ReplaceUnderscoreWithSpace
 exit 0
